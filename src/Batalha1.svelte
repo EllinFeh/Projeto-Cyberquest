908df836877d6cf703db0e38016fdfbd8a65391a
ruanzadaa <rlbl@discente.ifpe.edu.br>
2023-06-13 23:14:42 -0300

    adicionei varias coisas

--- src/Batalha1.svelte
+++ src/Batalha1.svelte
@@ -1,5 +1,20 @@
 <script>
     import { trocarEstadoDoJogo } from "./Estado.js";
+    import {Personagem} from "./personagens.js";
+    import {Adam} from "./personagens.js";
+
+let protagonista = new Adam(100,5,75)
+
+function ataq(){
+    protagonista.ataq
+}
+
+
 </script>
 
 <h1>Primeira Batalha</h1>
+
+
+
+
+

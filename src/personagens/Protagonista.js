c48543a9861ebd48916b15621c5c0df913c16929
arthurpip <tutucavieira1@gmail.com>
2023-07-01 17:06:31 -0300

    adicionado sistema de acerto e erro, besteira

--- src/personagens/Protagonista.js
+++ src/personagens/Protagonista.js
@@ -12,17 +12,35 @@ export class Adam extends personagem{
     }
     
     trirocket(ObjAtacado){
-        ObjAtacado.hp = ObjAtacado.hp - ((this.ataque) - ObjAtacado.def);
-        console.log ("trirocket foi usado!");
+        let dado1 = Math.floor(Math.random() * 15)
+        if (dado1 >= 4) {
+          ObjAtacado.hp = ObjAtacado.hp - ((this.ataque) - ObjAtacado.def);
+          console.log ("trirocket foi usado!");
+        }
+        else{
+            console.log ("você errou!");
+        }
     }
     
     spraytransfer(ObjAtacado){
-        ObjAtacado.hp = ObjAtacado.hp - ((Adam.atq * 2) - ObjAtacado.def)
+        let dado2 = Math.floor(Math.random() * 15)
+        if (dado2 >= 6) {
+        ObjAtacado.hp = ObjAtacado.hp - ((Adam.atq * 4.5) - ObjAtacado.def)
         console.log ("spraytransfer foi usado!")
+        }
+        else{
+            console.log ("você errou!");
+        }
     }
     
     medbay(){
+        let dado3 = Math.floor(Math.random() * 15)
+        if (dado3 >= 7) {
         return 70; 
+        }
+        else{
+            console.log ("você não conseguiu se curar!");
+        }
     }
 }
 
